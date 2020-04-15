43aae1d9707b25348d8d98cc1065c516f36e7b9a
akaashsanyal <akaashsanyal@gmail.com>
2020-04-15 16:17:04 -0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -5,5 +5,14 @@ import rectpack
 from rectpack import newPacker, float2dec
 from utils import * 
 
+data_file = ""
+dataset = read_dataset(data_file)
+for instance in dataset:
 
-
+    packer = newPacker(mode=PackingMode.Offline, \
+                      bin_algo=PackingBin.BBF, \
+                      pack_algo=MaxRectsBssf, \
+                      sort_algo=SORT_AREA, \
+                      rotation=True)
+    items = instance.items
+    bins = instance.binsize
