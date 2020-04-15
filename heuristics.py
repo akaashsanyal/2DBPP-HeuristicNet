450aea8cf7b8332ba2797c4ce6f5a636ba5e53c2
akaashsanyal <akaashsanyal@gmail.com>
2020-04-15 16:17:29 -0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -9,8 +9,7 @@ data_file = ""
 dataset = read_dataset(data_file)
 for instance in dataset:
 
-    packer = newPacker(mode=PackingMode.Offline, \
-                      bin_algo=PackingBin.BBF, \
+    packer = newPacker(bin_algo=PackingBin.BBF, \
                       pack_algo=MaxRectsBssf, \
                       sort_algo=SORT_AREA, \
                       rotation=True)
