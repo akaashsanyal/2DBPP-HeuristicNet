3db512efbbb544b7c170f936439dd70a9f9ee909
Akaash Sanyal <akaashsanyal@gmail.com>
2020-04-17 18:20:31 -0400

    started features

--- heuristics.py
+++ heuristics.py
@@ -1,9 +1,17 @@
+# Standard imports
 import sys
 import numpy as np
 import pandas as pd
-import rectpack
-from rectpack import newPacker, float2dec
 from utils import * 
+
+# Rectpack imports
+from rectpack import newPacker, float2dec
+from rectpack import PackingBin, PackingMode
+from rectpack import MaxRectsBl, MaxRectsBssf, MaxRectsBaf, MaxRectsBlsf
+from rectpack import SkylineMwf, SkylineMwfl, SkylineBl, \
+    SkylineBlWm, SkylineMwfWm, SkylineMwflWm
+from rectpack import SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE, \
+    SORT_LSIDE, SORT_RATIO, SORT_NONE
 
 bin_algos = [PackingBin.Global, PackingBin.BNF, PackingBin.BFF, \
                 PackingBin.BBF]
@@ -13,31 +21,39 @@ pack_algos = [MaxRectsBl, MaxRectsBssf, MaxRectsBaf, MaxRectsBlsf, \
 sort_algos = [SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE, \
                 SORT_LSIDE, SORT_RATIO, SORT_NONE]
 
-data_file = ""
+# Read in dataset
+data_file = sys.argv[1]
 dataset = read_dataset(data_file)
 
 # Repeat for each heuristic
 for instance in dataset:
-
     # Initialize Packer
-    packer = newPacker(bin_algo=PackingBin.BBF, \
-                      pack_algo=MaxRectsBssf, \
-                      sort_algo=SORT_AREA, \
-                      rotation=True)
+    packer = newPacker(mode=PackingMode.Offline, \
+                    bin_algo=PackingBin.BBF, \
+                    pack_algo=MaxRectsBssf, \
+                    sort_algo=SORT_AREA, \
+                    rotation=True)
     
     # Add bins and items to Packer
     items = instance.items
     bins = instance.binsize
     for i in items:
-        packer.add_rect(i)
+        packer.add_rect(*i)
     packer.add_bin(bins[0], bins[1], count=float("inf"))
-
+    
     # Start packing
     packer.pack()
 
     # Full item list
     all_rects = packer.rect_list()
-
-    # Evaluate performance
-
+'''    
+    # Evaluate performance   
+    print("INSTANCE RESULTS")
+    for rect in all_rects:
+        print(rect)
+'''
     # Save results
+
+
+
+print(generate_features(dataset))
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import numpy as np
 
 # Instance class
 class Instance:
@@ -19,9 +20,10 @@ class Instance:
             print(item)
 
 # Make dataset and save to file
-def generate_dataset(filepath):
+def generate_raw_dataset(filepath):
     '''
     Format is as follows:
+    instance number
     bin_length bin_width
     item_length item_width item_id
     item_length item_width item_id
@@ -58,4 +60,46 @@ def read_dataset(filepath):
                 i.add_item(tuple(spl_int))
     rf.close()
     return instance_list
+
+# Generate features from dataset
+def generate_features(dataset):
+    
+    feature_space = []
+    # Loops over each instance
+    for i in dataset:
+        # Generate features
+        features = []
+        
+        # Item features
+        vols = [item[0]*item[1] for item in i.items]
+        print(vols)
+        # Number of items
+        features.append(len(i.items))
+
+        # Sum of item volumes
+        features.append(sum(vols))
+
+        # Min item volume
+        features.append(min(vols))
+
+        # Max item volume
+        features.append(max(vols))
+
+        # Average item volume
+        features.append(np.mean(vols))
+
+        # Standard deviation of item volumes
+        features.append(np.std(vols))
+
+        # Variance of item volumes
+        features.append(np.var(vols))
+       
+        '''
+        Idk add others
+        '''
+        print(features)
+        # Add to final feature labels
+        feature_space.append(features) 
+
+    return feature_space
 
