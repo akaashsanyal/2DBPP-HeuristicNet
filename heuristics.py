ef35ef73a7985425ed2c0dd7a5283767f1463b3d
akaashsanyal <akaashsanyal@gmail.com>
2020-04-20 01:31:20 -0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -2,7 +2,6 @@
 import sys
 import numpy as np
 import pandas as pd
-from utils import * 
 
 # Rectpack imports
 from rectpack import newPacker, float2dec
@@ -13,45 +12,44 @@ from rectpack import SkylineMwf, SkylineMwfl, SkylineBl, \
 from rectpack import SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE, \
     SORT_LSIDE, SORT_RATIO, SORT_NONE
 
-bin_algos = [PackingBin.Global, PackingBin.BNF, PackingBin.BFF, \
-                PackingBin.BBF]
-pack_algos = [MaxRectsBl, MaxRectsBssf, MaxRectsBaf, MaxRectsBlsf, \
-                SkylineMwf, SkylineMwfl, SkylineBl, \
-                SkylineBlWm, SkylineMwfWm, SkylineMwflWm]
-sort_algos = [SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE, \
-                SORT_LSIDE, SORT_RATIO, SORT_NONE]
+def generate_labels(dataset):
+    bin_algos = [PackingBin.Global, PackingBin.BNF, PackingBin.BFF, \
+                    PackingBin.BBF]
+    pack_algos = [MaxRectsBl, MaxRectsBssf, MaxRectsBaf, MaxRectsBlsf, \
+                    SkylineMwf, SkylineMwfl, SkylineBl, \
+                    SkylineBlWm, SkylineMwfWm, SkylineMwflWm]
+    sort_algos = [SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE, \
+                    SORT_LSIDE, SORT_RATIO, SORT_NONE]
+    labels = []
+    # Repeat for each heuristic
+    for instance in dataset:
+        # Initialize Packer
+        packer = newPacker(mode=PackingMode.Offline, \
+                        bin_algo=PackingBin.BBF, \
+                        pack_algo=MaxRectsBssf, \
+                        sort_algo=SORT_AREA, \
+                        rotation=True)
 
-# Read in dataset
-data_file = sys.argv[1]
-dataset = read_dataset(data_file)
+        # Add bins and items to Packer
+        items = instance.items
+        bins = instance.binsize
+        for i in items:
+            packer.add_rect(*i)
+        packer.add_bin(bins[0], bins[1], count=float("inf"))
 
-# Repeat for each heuristic
-for instance in dataset:
-    # Initialize Packer
-    packer = newPacker(mode=PackingMode.Offline, \
-                    bin_algo=PackingBin.BBF, \
-                    pack_algo=MaxRectsBssf, \
-                    sort_algo=SORT_AREA, \
-                    rotation=True)
+        # Start packing
+        packer.pack()
+
+        # Full item list
+        all_rects = packer.rect_list()
+    '''    
+        # Evaluate performance   
+        print("INSTANCE RESULTS")
+        for rect in all_rects:
+            print(rect)
+    '''
+        # Save results
+        # something like labels.append(list of heuristic rankings, or just a number)
     
-    # Add bins and items to Packer
-    items = instance.items
-    bins = instance.binsize
-    for i in items:
-        packer.add_rect(*i)
-    packer.add_bin(bins[0], bins[1], count=float("inf"))
-    
-    # Start packing
-    packer.pack()
-
-    # Full item list
-    all_rects = packer.rect_list()
-'''    
-    # Evaluate performance   
-    print("INSTANCE RESULTS")
-    for rect in all_rects:
-        print(rect)
-'''
-    # Save results
-
+    return labels
 
