72a8fcb56a11c8f5f6e4b6bd9d2f0af03b2e4588
akaashsanyal <akaashsanyal@gmail.com>
2020-04-21 11:55:07 -0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -16,38 +16,36 @@ def generate_labels(dataset):
     labels = []
     # Repeat for each heuristic
     for instance in dataset:
+        instance_label = []
         for bin_algo in bin_algos:
             for pack_algo in pack_algos:
-        # Initialize Packer
-        packer = newPacker(mode=PackingMode.Offline, \
-                        bin_algo=PackingBin.BBF, \
-                        pack_algo=MaxRectsBssf, \
-                        sort_algo=SORT_RATIO, \
-                        rotation=True)
+                # Initialize Packer
+                packer = newPacker(mode=PackingMode.Offline, \
+                                bin_algo=PackingBin.BBF, \
+                                pack_algo=MaxRectsBssf, \
+                                sort_algo=SORT_RATIO, \
+                                rotation=True)
 
-        # Add bins and items to Packer
-        items = instance.items
-        bins = instance.binsize
-        for i in items:
-            packer.add_rect(*i)
-        packer.add_bin(bins[0], bins[1], count=float("inf"))
+                # Add bins and items to Packer
+                items = instance.items
+                bins = instance.binsize
+                for i in items:
+                    packer.add_rect(*i)
+                packer.add_bin(bins[0], bins[1], count=float("inf"))
 
-        # Start packing
-        packer.pack()
+                # Start packing
+                packer.pack()
 
-        # Full item list
-        all_rects = packer.rect_list()
-        
-        # Evaluate performance
+                # Full item list
+                all_rects = packer.rect_list()
 
-    '''    
-        # Evaluate performance   
-        print("INSTANCE RESULTS")
-        for rect in all_rects:
-            print(rect)
-    '''
+                # Evaluate performance
+                # Count number of bins
+                instance_label.append(1)
+                
+                
         # Save results
         # something like labels.append(list of heuristic rankings, or just a number)
-    
+        labels.append(instance_label)
     return labels
 
