c83764c3ef9a08aa1dc18734a9a005d474b9e946
akaashsanyal <akaashsanyal@gmail.com>
2020-04-22 18:31:46 -0400

    Update net.py

--- net.py
+++ net.py
@@ -4,11 +4,8 @@ import pickle
 import keras
 from keras.models import Model, Sequential
 from keras.layers import Dropout, Dense, BatchNormalization
-from keras.callbacks import ModelCheckpoint
-from utils import *
-from heuristics import *
 
-def train(features_file, labels_file, model_file):
+def train(features_file, labels_file, model_file, epoch_num):
     features, num_features = pickle.load(open("features.txt", 'rb'))
     labels, num_heuristics = pickle.load(open("labels.txt", 'rb'))
     
@@ -35,8 +32,6 @@ def train(features_file, labels_file, model_file):
         loss='categorical_crossentropy', \
         metrics=['accuracy'])
 
-    checkpointer = ModelCheckpoint(filepath= 'model_epoch{epoch:02d}.h5', verbose=1)
-
-    model.fit(features, labels, epochs=50, batch_size=32)
+    model.fit(features, labels, epochs=epoch_num, batch_size=32)
 
     model.save(model_file)
